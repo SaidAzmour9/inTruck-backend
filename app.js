dabc6fadf546ee68939159eb9c6e3b785fd7381f
Said Azmour <azmour2016maroc@gmail.com>
2025-04-10 20:56:31 +0100

    add locall

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 
 
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
