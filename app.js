aabc2d3459e1a2ecafbd4ca3b40539d836dc1492
Bensalem Hamza <bensalemhamza382@gmail.com>
2025-04-21 20:54:31 +0200

    update cors

--- app.js
+++ app.js
@@ -11,13 +11,25 @@ const cors = require('cors');
 
 
 
-app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://in-truck-client-px4uaenfj-hamza-bensalems-projects.vercel.app'],
+const allowedOrigins = [
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'https://in-truck-client-px4uaenfj-hamza-bensalems-projects.vercel.app'
+  ];
+  
+  app.use(cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (e.g., mobile apps, Postman)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true); // Allow the origin
+      } else {
+        callback(new Error('Not allowed by CORS')); // Reject the request
+      }
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-}));
-
+    credentials: true // Allow sending credentials (cookies)
+  }));
 
 app.use(express.json());
 app.use(cookieParser());
