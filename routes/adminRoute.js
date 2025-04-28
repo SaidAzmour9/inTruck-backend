204b0e29d94254f530b06114bd8a03614a5b59a2
Said Azmour <azmour2016maroc@gmail.com>
2025-04-28 15:39:22 +0100

    get available trucks and

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -21,8 +21,8 @@ router.put('/trucks/:id/edit', admin.updateTruck);
 router.delete('/trucks/:id/delete', admin.deleteTruck);
 //assignDriverToTruck
 router.put('/trucks/:id/assign-driver', admin.assignDriverToTruck);
-//get  trucks
-router.get('/trucks/available', admin.getAvailableTrucks);
+//get available trucks
+router.get('/trucks/available', admin.getTrucks);
 
 // Drivers
 router.get('/drivers', admin.getDrivers);
@@ -31,7 +31,7 @@ router.post('/drivers/add', admin.addDriver);
 router.put('/drivers/:id/edit', admin.updateDriver);
 router.delete('/drivers/:id/delete', admin.deleteDriver);
 //get available drivers
-router.get('/drivers/available', admin.getDrivers);
+router.get('/drivers/available', admin.getAvailableDrivers);
 
 
 // Orders
